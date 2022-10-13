82d80b4fdcd5b1cb0199e87c02e0d2139bab22a0
lu78abranches <luisabranches.surf@hotmail.com>
2022-10-13 00:15:01 -0300

    exemplos classe TipoVariaveis.  classe Operadores

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,32 @@
+import javax.swing.event.SwingPropertyChangeSupport;
+
+public class Operadores {
+    public static void main(String[] args) {
+        String nomeCompleto = "LINGUAGEM" + "JAVA";
+
+        System.out.println(nomeCompleto);
+
+        // qual o resultado das expressoes abaixo?
+        String concatenacao = "?";
+
+        concatenacao = 1 + 1 + 1 + "1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1 + "1" + 1 + 1;
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1 + "1" + 1 + "1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = "1" + 1 + 1 + 1;
+
+        System.out.println(concatenacao);
+
+        concatenacao = "1" + (1 + 1 + 1);
+
+        System.out.println(concatenacao);
+    }
+}
--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -3,8 +3,22 @@ public class TiposVariaveis {
 
         // TiposEVariaveis.java
 
+        // estude tipos primitivos byte, short, int, long, double, float(com casas
+        // decimais)
+        // estude aclasse String que representa texto na aplição
+
+        String nome = "LUIS ABRANCHES";
+
         short numeroCurto = 2;
         int numeroNormal = numeroCurto;
         short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+        numero = 10;
+
+        System.out.print(numero);
+
+        final double VALOR_DE_PI = 3.14;
+
     }
 }
